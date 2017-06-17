5c9094bbc90c565ca40d7e5072feb4bc94a594fb
flouk19 <flouk19@users.noreply.github.com>
2017-06-17 21:37:18 +0200

    Rename the module name to vote-azap and add a basic (nonworking) implementation of a order module

--- client/main.js
+++ client/main.js
@@ -5,24 +5,26 @@ import angularMeteor from 'angular-meteor';
 import addVote from '../imports/components/vote/addVote';
 import voteComp from '../imports/components/voteComp/voteComp';
 import vote from '../imports/components/vote/vote';
+import order from '../imports/components/order/order';
 import '../imports/startup/accounts-config.js';
 import '../imports/other/angular-bootstrap-datetimepicker-directive.js';
 import Chart from 'chart.js/src/chart.js';
 import chartjs from 'angular-chart.js';
 
-angular.module('simple-todos', [
+angular.module('vote-azap', [
   angularMeteor,
   'angular.filter',
   'datetimepicker',
-  vote.name,
   'accounts.ui',
+  vote.name,
   voteComp.name,
   addVote.name,
+  order.name,
   'chart.js'
 ]);
 
 function onReady() {
-    angular.bootstrap(document, ['simple-todos']);
+    angular.bootstrap(document, ['vote-azap']);
 }
 
 if(Meteor.isCordova){
